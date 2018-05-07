00d8331c50af20c0e5977569439fbd0a37eb621e
Lev Rechnik <relev@pisem.net>
2018-05-07 17:59:18 +0300

    v2

--- lib/omniauth/strategies/identity.rb
+++ lib/omniauth/strategies/identity.rb
@@ -13,14 +13,19 @@ module OmniAuth
       option :locate_conditions, lambda{|req| Rails.logger.debug [:locate_conditions, model, req['login']]; {model.auth_key => req['login']} }
 
       def request_phase
+        request_form
+      end
+
+      def request_form
         if options[:on_login]
           options[:on_login].call(self.env)
         else
           OmniAuth::Form.build(
-            :title => (options[:title] || "Sign in"),
-            :url => registration_path
+            title: (options[:title] || "Sign in"),
+            url: confirmation_path,
           ) do |f|
             f.text_field 'Login', 'login'
+              f.button 'Sign in'
           end.to_response
         end
       end
@@ -32,10 +37,12 @@ module OmniAuth
       end
 
       def other_phase
-        if on_registration_path?
-          if request.get?
-            registration_form
-          elsif request.post?
+        if on_confirmation_path?
+          if request.post?
+            confirmation_phase
+          end
+        elsif on_registration_path?
+          if request.post?
             registration_phase
           end
         else
@@ -43,25 +50,45 @@ module OmniAuth
         end
       end
 
-      def registration_form
-        if options[:on_registration]
-          options[:on_registration].call(self.env)
+      def confirmation_form(args={})
+        if options[:on_confirmation]
+          options[:on_confirmation].call(self.env)
         else
           OmniAuth::Form.build(
             :title => (options[:title] || "Confirm"),
-            :url => callback_path
+            :url => registration_path
             ) do |f|
-            f.text_field 'Password', 'password'
+              f.text_field 'Password', 'password'
+              f.label_field args[:error], :error if args[:error]
+              f.button 'Confirm'
           end.to_response
         end
       end
 
-      def registration_phase
-        attributes = (options[:fields] + []).inject({}){|h,k| h[k] = request[k.to_s]; h}
-        attributes[:password] = '123'
+      def confirmation_phase
+        login = request['login']
+        return request_form unless login
+
+        attributes = {
+          login: login,
+          password: '123',
+        }
+
+        Rails.logger.debug [:confirmation_phase, login, attributes, model]
+
         @identity = model.create(attributes)
-        Rails.logger.debug [:registration_phase, @identity]
+        Rails.logger.debug [:confirmation_phase, @identity]
+
         if @identity.persisted?
+          session['omniauth.identity_id'] = @identity.id
+          confirmation_form
+        end
+      end
+
+      def registration_phase
+        @identity = model.find(session['omniauth.identity_id'])
+        if @identity.authenticate(request['password'])
+          session['omniauth.identity_id'] = nil
           env['PATH_INFO'] = callback_path
           callback_phase
         else
@@ -69,7 +96,7 @@ module OmniAuth
             self.env['omniauth.identity'] = @identity
             options[:on_failed_registration].call(self.env)
           else
-            registration_form
+            confirmation_form(error: 'Неверный код авторизации')
           end
         end
       end
@@ -77,23 +104,22 @@ module OmniAuth
       uid{ identity.uid }
       info{ identity.info }
 
+      def confirmation_path
+        options[:confirmation_path] || "#{path_prefix}/#{name}/confirm"
+      end
+      def on_confirmation_path?
+        on_path?(confirmation_path)
+      end
       def registration_path
         options[:registration_path] || "#{path_prefix}/#{name}/register"
       end
-
       def on_registration_path?
         on_path?(registration_path)
       end
 
       def identity
-        if options.locate_conditions.is_a? Proc
-          Rails.logger.debug [:request, request.params, options]
-          conditions = instance_exec(request, &options.locate_conditions)
-          conditions.to_hash
-        else
-          conditions = options.locate_conditions.to_hash
-        end
-        Rails.logger.debug [:identity, conditions, request['password']]
+        Rails.logger.debug [:identity, request['password'], session['omniauth.identity']]
+        conditions = {id: session['omniauth.identity']}
         @identity ||= model.authenticate(conditions, request['password'])
       end
 
